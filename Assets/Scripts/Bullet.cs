3e12e0aa8e764b63fe0b5281348279e84dae8d4e
VictorGrondin <grondin.victor85@gmail.com>
2023-10-25 14:00:44 +0200

    hyhhh

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D monRigidBody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monRigidBody.velocity = Vector3.up*speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
+
+}
